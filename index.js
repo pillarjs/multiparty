6d66ebeb69e892667d916afefb5c30bc6043c3d6
Serhii <sergey.anistratenko@popel.com.ua>
2021-04-27 18:51:18 +0300

    whitespaces support

--- index.js
+++ index.js
@@ -795,9 +795,9 @@ function uploadPath(baseDir, filename) {
 }
 
 function parseFilename(headerValue) {
-  var m = headerValue.match(/\bfilename="(.*?)"($|; )/i);
+  var m = headerValue.match(/\bfilename="((.|\s)*?)"($|; )/i);
   if (!m) {
-    m = headerValue.match(/\bfilename\*=utf-8''(.*?)($|; )/i)
+    m = headerValue.match(/\bfilename\*=utf-8''((.|\s)*?)($|; )/i)
     if (m) {
       m[1] = decodeURI(m[1]);
     } else {
